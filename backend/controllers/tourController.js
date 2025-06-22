0967755edb05110f81b9a11bd294dbf21402b7ed
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2025-06-22 11:18:43 +0530

    add search tour integration

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -113,17 +113,23 @@ export const getAllTour = async (req, res) => {
 // get tour by search
 export const getTourBySearch = async (req, res) => {
   // here 'i' means case sensitive
-  const city = new RegExp(req.query.city, "i");
+  // const city = new RegExp(req.query.city, "i");
+  const city = req.query.city;
+
   const distance = parseInt(req.query.distance);
   const maxGroupSize = parseInt(req.query.maxGroupSize);
 
   try {
     // gte means greater
     const tours = await Tour.find({
-      city,
+      // city,
+      city: { $regex: city, $options: "i" },
       distance: { $gte: distance },
       maxGroupSize: { $gte: maxGroupSize },
+      // distance: { $lte: distance }, // FIXED
+      // maxGroupSize: { $lte: maxGroupSize },
     }).populate("reviews");
+
     res.status(200).json({
       success: true,
       count: tours.length,
--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -1,6 +1,9 @@
 import React, {useRef} from 'react'
 import './search-bar.css'
 import { Col, Form, FormGroup } from 'reactstrap'
+import { BASE_URL } from './../utils/config'
+import { useNavigate } from 'react-router-dom';
+
 
 
 const SearchBar = () => {
@@ -8,8 +11,9 @@ const SearchBar = () => {
     const locationRef = useRef('')
     const distanceRef = useRef(0)
     const maxGroupSizeRef = useRef(0)
+    const navigate = useNavigate()
 
-    const searchHandler = () => {
+    const searchHandler = async() => {
         const location = locationRef.current.value
         const distance = distanceRef.current.value
         const maxGroupSize = maxGroupSizeRef.current.value
@@ -17,9 +21,19 @@ const SearchBar = () => {
         if(location === '' || distance === '' || maxGroupSize === ''){
             alert("All fields are required!");
         }
+
+        const res = await fetch(`${BASE_URL}/tours/search/getTourBySearch?
+            city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`)
+        
+        if(!res.ok) alert('Something went wrong')
+        
+        const result = await res.json()
+        navigate(`/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`,
+            { state: result.data }
+        )
     }
 
-  return (<Col lg='12'>
+    return (<Col lg='12'>
         <div className="search__bar">
             <Form className="d-flex align-items-center gap-4">
                 <FormGroup className="d-flex gap-3 form__group form__group-fast">
